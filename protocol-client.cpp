002ea222ab367333668126792bcaab102f148c20
Bartek Parapura <b.parapura@student.uw.edu.pl>
2025-06-05 08:34:16 +0200

    getting coeffs

--- protocol-client.cpp
+++ protocol-client.cpp
@@ -55,7 +55,7 @@ std::vector<double> send_hello(int server_fd, const std::string& id) {
         if (checkCoeff(coeffs)) {
             break;
         } else {
-            std::cout << "here2" << std::endl;
+            std::cout << "here2 " << coeffs << " " << std::endl;
             print_bad_msg(server_fd, coeffs);
             coeffs = read_msg(server_fd);
         }
