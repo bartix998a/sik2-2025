40b8f1571ef05bbe4abd8980e890b797c5b23879
Bartek Parapura <b.parapura@student.uw.edu.pl>
2025-06-05 09:05:59 +0200

    getting coeffs

--- coeffs/coeff_gen.py
+++ coeffs/coeff_gen.py
@@ -1,11 +1,13 @@
 import random
 
 def write_random_rational_pairs(filename):
-    with open(filename, 'w') as f:
+    with open(filename, 'wb') as f:
         for _ in range(5):
             # Generate two random rational numbers
             num1 = random.uniform(-100., 100)
             num2 = random.uniform(-100., 100)
             # Write to file with required format
-            f.write(f"COEFFS {num1:.7f} {num2:.7f}\r\n")
+            f.write(f"COEFFS {num1:.7f} {num2:.7f}".encode('ascii'))
+            f.write("\r".encode('ascii'))
+            f.write("\n".encode('ascii'))
 write_random_rational_pairs("coeffs.txt")
