348b5fceb65bfea67ad5277df6851fdba3fc8652
Bartek Parapura <b.parapura@student.uw.edu.pl>
2025-06-06 17:38:08 +0200

    getting coeffs

--- common2.cpp
+++ common2.cpp
@@ -41,5 +41,5 @@ std::string print_ip_info(int fd) {
         syserr("getnameinfo");
     }
 
-    return (addr.sin6_family == AF_INET6 ? host : host.substr(7)) + ":" + port;
+    return (IN6_IS_ADDR_V4MAPPED(&addr.sin6_addr) ? host.substr(7) : host) + ":" + port;
 }
